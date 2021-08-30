edbb6833d53c924e8fd3ebf34b1f67483214522d
han3000 <nguyenhan@pm.me>
2021-08-30 10:50:14 -0700

    Add current updated files

---
+++ includes/classes/Constants.php
@@ -0,0 +1,16 @@
+<?php
+
+class Constants {
+    public static $firstNameCharacters = "Your first name must be between 2 and 25 characters";
+    public static $lastNameCharacters = "Your last name must be between 2 and 25 characters";
+    public static $usernameCharacters = "Your username must be between 2 and 25 characters";
+    public static $usernameTaken = "Username already in use";
+    public static $emailsDoNotMatch = "Your emails do not match";
+    public static $emailInvalid = "Invalid email. Check if it includes @ symbol, etc.";
+    public static $emailRegistered = "Email is already register";
+    public static $passwordsDoNotMatch = "Passwords do not match";
+    public static $passwordLengthInvalid = "Your password must be between 5 and 25 characters";
+    public static $loginFailed = "Incorrect username or password";
+}
+
+?>
---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php
+
+ob_start(); // Turns on output buffering
+session_start();
+
+date_default_timezone_set("American/Los_Angeles");
+
+try {
+    $con = new PDO("mysql:dbname=hflix;host=localhost", "root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+catch (PDOException $e) {
+    exit("Connection failed: " . $e->getMessage());
+}
+
+?>
